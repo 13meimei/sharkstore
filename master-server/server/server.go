6df3c9ebaa0da20c5ad8feb76adbb9dabb2c5c79
Your Name <you@example.com>
2018-06-15 11:54:55 +0800

    ms: lead change assign alarmClient

--- master-server/server/server.go
+++ master-server/server/server.go
@@ -337,6 +337,7 @@ func (service *Server) RaftLeaderChange(leaderId uint64) {
 		cluster.UpdateLeader(raftLeader)
 		cluster.Start()
 		service.cluster = cluster
+		service.cluster.alarmCli = service.alarmClient
 		return
 	}
 
