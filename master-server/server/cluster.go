5117101da5a467d2f6fd4f41d1202e11edf4e6cf
maggieWmj <maggiewu.64@gmail.com>
2018-06-28 20:04:50 +0800

    ms. update switch init

--- master-server/server/cluster.go
+++ master-server/server/cluster.go
@@ -620,8 +620,7 @@ func (c *Cluster) GetAllTasks() []*TaskChain {
 }
 
 func (c *Cluster) loadAutoTransfer() error {
-	var s uint64
-	s = uint64(1)
+	s := uint64(0)
 	key := fmt.Sprintf(PREFIX_AUTO_TRANSFER, c.clusterId)
 	value, err := c.store.Get([]byte(key))
 	if err != nil {
@@ -631,24 +630,22 @@ func (c *Cluster) loadAutoTransfer() error {
 			return err
 		}
 	}
-	var auto bool
-	auto = true // enable by default
+	var auto bool  // enable by default
 	if value != nil {
 		s, err = bytesToUint64(value)
 		if err != nil {
 			return err
 		}
 	}
-	if s == 0 {
-		auto = false
+	if s == 1 {
+		auto = true
 	}
 	c.autoTransferUnable = auto
 	return nil
 }
 
 func (c *Cluster) loadAutoFailover() error {
-	var s uint64
-	s = uint64(1)
+	s := uint64(0)
 	key := fmt.Sprintf(PREFIX_AUTO_FAILOVER, c.clusterId)
 	value, err := c.store.Get([]byte(key))
 	if err != nil {
@@ -659,23 +656,21 @@ func (c *Cluster) loadAutoFailover() error {
 		}
 	}
 	var auto bool
-	auto = true
 	if value != nil {
 		s, err = bytesToUint64(value)
 		if err != nil {
 			return err
 		}
 	}
-	if s == 0 {
-		auto = false
+	if s == 1 {
+		auto = true
 	}
 	c.autoFailoverUnable = auto
 	return nil
 }
 
 func (c *Cluster) loadAutoSplit() error {
-	var s uint64
-	s = uint64(1)
+	s := uint64(0)
 	key := fmt.Sprintf(PREFIX_AUTO_SPLIT, c.clusterId)
 	value, err := c.store.Get([]byte(key))
 	if err != nil {
@@ -686,15 +681,14 @@ func (c *Cluster) loadAutoSplit() error {
 		}
 	}
 	var auto bool
-	auto = true // enable by default
 	if value != nil {
 		s, err = bytesToUint64(value)
 		if err != nil {
 			return err
 		}
 	}
-	if s == 0 {
-		auto = false
+	if s == 1 {
+		auto = true
 	}
 	c.autoSplitUnable = auto
 	return nil
