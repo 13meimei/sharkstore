b7e95310d0d002d5335f3aeee536649a361d7bdd
zhangyongcheng <zhangyongcheng2@jd.com>
2018-07-16 18:10:40 +0800

    modify

--- data-server/src/range/watch.cpp
+++ data-server/src/range/watch.cpp
@@ -38,12 +38,12 @@ bool DecodeWatchKey(std::vector<std::string *> &keys, std::string *buf) {
 
     size_t offset;
     for (offset = 9; offset != buf->length() - 1;) {
-        auto b = std::make_unique<std::string>();
+        auto b = std::make_shared<std::string>();
 
-        if (!DecodeBytesAscending(*buf, offset, b)) {
+        if (!DecodeBytesAscending(*buf, offset, b.get())) {
             return false;
         }
-        keys.push_back(b);
+        keys.push_back(b.get());
     }
     return true;
 }
@@ -57,7 +57,7 @@ void EncodeWatchValue(std::string *buf,
     EncodeBytesValue(buf, 4, extend->c_str(), extend->length());
 }
 
-bool DecodeWatchValue(uint64_t *version, std::string *value, std::string *extend,
+bool DecodeWatchValue(int64_t *version, std::string *value, std::string *extend,
                       std::string &buf) {
     assert(version != nullptr && value != nullptr && extend != nullptr &&
            buf.length() != 0);
@@ -76,7 +76,7 @@ void WatcherSet::AddWatcher(std::string &name, common::ProtoMessage *msg) {
     auto kit0 = key_index_.find(name);
     if (kit0 == key_index_.end()) {
         auto tmpPair = key_index_.emplace(std::make_pair(name, std::make_pair(msg->session_id, msg)));
-        if (tmpPair->second) kit0 = tmpPair->first;
+        if (tmpPair.second) kit0 = tmpPair.first;
         else FLOG_DEBUG("AddWatcher abnormal key:%s session_id:%lld .", name.data(), msg->session_id);
     }
 
@@ -89,7 +89,7 @@ void WatcherSet::AddWatcher(std::string &name, common::ProtoMessage *msg) {
         auto wit0 = watcher_index_.find(msg->session_id);
         if (wit0 == watcher_index_.end()) {
             auto tmpPair = watcher_index_.emplace(std::make_pair(msg->session_id, std::make_pair(name, nullptr)));
-            if (tmpPair->second) wit0 = tmpPair->first;
+            if (tmpPair.second) wit0 = tmpPair.first;
             else FLOG_DEBUG("AddWatcher abnormal session_id:%lld key:%s .", msg->session_id, name.data());
         }
 
@@ -123,7 +123,7 @@ WATCH_CODE WatcherSet::DelWatcher(const int64_t &id, const std::string &key) {
             auto itSession = itKeyIdx->second.find(id);
 
             if (itSession != itKeyIdx->second.end()) {
-                itKeyIdx->second.erase(itSession)
+                itKeyIdx->second.erase(itSession);
             }
             key_index_.erase(itKeyIdx);
         }
@@ -147,7 +147,7 @@ uint32_t WatcherSet::GetWatchers(std::vector<common::ProtoMessage *> &vec, const
     }
 
     auto watchers = kit->second;
-    uint32_t msgSize = watchers.size();
+    uint32_t msgSize = static_cast<uint32_t>(watchers.size());
 
     if (msgSize > 0) {
         vec.resize(msgSize);
@@ -160,5 +160,104 @@ uint32_t WatcherSet::GetWatchers(std::vector<common::ProtoMessage *> &vec, const
     return msgSize;
 }
 
+int16_t WatchCode::EncodeKv(funcpb::FunctionID funcId, const metapb::Range &meta_, watchpb::WatchKeyValue *kv, 
+                            std::string &db_key, std::string &db_value,
+                            errorpb::Error *err) {
+        int16_t ret(0);
+        std::vector<std::string*> keys;
+        std::string funcName("");
+        std::string ext("");
+        keys.clear();
+        db_key.clear();
 
+        switch (funcId) {
+            case funcpb::kFuncWatchGet:
+            case funcpb::kFuncPureGet:
+            case funcpb::kFuncWatchPut:
+            case funcpb::kFuncWatchDel:
+                if (funcId == funcpb::kFuncWatchGet) {
+                    funcName.assign("WatchGet");
+                } else if (funcId == funcpb::kFuncPureGet) {
+                    funcName.assign("PureGet");
+                } else if (funcId == funcpb::kFuncWatchPut) {
+                    funcName.assign("WatchPut");
+                } else {
+                    funcName.assign("WatchDel");
+                }
+
+                for (auto i = 0; i < kv->key_size(); i++) {
+                    keys.push_back(kv->mutable_key(i));
+
+                    FLOG_DEBUG("range[%"PRIu64"] %s key%d):%s", meta_.id(), funcName.data(), i, kv->mutable_key(i)->data());
+                }
+
+                if(kv->key_size()) {
+                    EncodeWatchKey(&db_key, meta_.table_id(), keys);
+                } else {
+                    ret = -1;
+                    if (db_key.empty() || kv->key_size() < 1) {
+                        FLOG_WARN("range[%" PRIu64 "] %s error: key empty", meta_.id(), funcName.data());
+                        //err = errorpb::KeyNotInRange(db_key);
+                        break;
+                    }
+                }
+                FLOG_DEBUG("range[%" PRIu64 "] %s info: table_id:%lld key before:%s after:%s", 
+                           meta_.id(), funcName.data(), meta_.table_id(),  keys[0]->data(), db_key.data());
+
+                if (!kv->value().empty()) {
+                    int64_t tmpVersion = kv->version();
+                    EncodeWatchValue( &db_value, tmpVersion, kv->mutable_value(), &ext);
+                }
+                FLOG_DEBUG("range[%" PRIu64 "] %s info: value before:%s after:%s", 
+                           meta_.id(), funcName.data(), kv->value().data(), db_value.data());
+                break;
+            
+            default:
+                ret = -1;
+                err->set_message("unknown func_id");
+                FLOG_WARN("range[%" PRIu64 "] %s error: unknown func_id:%d", meta_.id(), funcId);
+                break;
+        }
+        return ret;
+    }
+
+int16_t WatchCode::DecodeKv(funcpb::FunctionID funcId, const metapb::Range &meta_, watchpb::WatchKeyValue *kv, 
+                            std::string &db_key, std::string &db_value,
+                            errorpb::Error *err) {
+        int16_t ret(0);
+        std::vector<std::string*> keys;
+        int64_t version(0);
+        keys.clear();
+        
+        auto val = std::make_shared<std::string>("");
+        auto ext = std::make_shared<std::string>("");
+
+        switch (funcId) {
+            case funcpb::kFuncWatchGet:
+            case funcpb::kFuncPureGet:
+            case funcpb::kFuncWatchPut:
+            case funcpb::kFuncWatchDel:
+                //decode value
+                DecodeWatchValue(&version, val.get(), ext.get(), db_value);
+                kv->set_key(0, db_key);
+                kv->set_value(*val);
+                kv->set_version(version);
+                kv->set_ext(*ext);
+                break;
+            
+            default:
+                ret = -1;
+                if (err == nullptr) {
+                    err = new errorpb::Error;
+                }
+                err->set_message("unknown func_id");
+                FLOG_WARN("range[%" PRIu64 "] %s error: unknown func_id:%d", meta_.id(), funcId);
+                break;
+        }
+        return ret;
+}
+
+} //end namespace range
+} //end namespace data-server
+} //end namespace sharkstore
 
