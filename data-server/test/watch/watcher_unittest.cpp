b154d8896b1320dc0bcebd87b6028a0855897800
Your Name <you@example.com>
2018-08-02 14:33:55 +0800

    improve watcher unit test

--- data-server/test/watch/watcher_unittest.cpp
+++ data-server/test/watch/watcher_unittest.cpp
@@ -23,14 +23,15 @@ enum TestCode {
     TEST_OK = 0,
     TEST_ERR,
     TEST_TIMEOUT,
+    TEST_UNEXPECTED,
 };
 
 class TestWatchConnection: public common::ProtoMessage {
 public:
     TestWatchConnection() = delete;
-    explicit TestWatchConnection(int timeout): timeout_(timeout), ProtoMessage_(timeout-1000) {
+    explicit TestWatchConnection(int timeout): timeout_(timeout+1000), ProtoMessage_(timeout) { // request timeout > server timeout 1s
         auto ret = socketpair(AF_UNIX, SOCK_STREAM, 0, sock_);
-        assert(ret == 0);
+
         /*
         char *write_buf = "A";
         char read_buf[2] = {0};
@@ -39,6 +40,12 @@ public:
         read(sock_[0], read_buf, 1);
         printf("test buf: %s\n", read_buf);
         */
+    }
+    TestWatchConnection(int timeout, const char* send_string, const char* expect_string):
+            timeout_(timeout+1000), ProtoMessage_(timeout) {
+        memcpy(send_string_, send_string, strlen(send_string) > sizeof(send_string_)-1 ? sizeof(send_string_)-1 : strlen(send_string));
+        memcpy(expect_string_, expect_string, strlen(expect_string) > sizeof(expect_string_)-1 ? sizeof(expect_string_)-1 : strlen(expect_string));
+        auto ret = socketpair(AF_UNIX, SOCK_STREAM, 0, sock_);
     }
 
 public:
@@ -52,23 +59,34 @@ public:
     }
     void ClientWrite() {
         std::cout << "client write ... " << std::endl;
-        return Write(0);
+        Write(0, send_string_);
+        return;
     }
     void ServerWrite() {
         std::cout << "server write ... " << std::endl;
-        return Write(1);
+
+        auto expire = expire_time/1000;
+        auto now = getticks()/1000;
+        if (expire <= now) {
+            std::cout << "expire time(sec): " << expire << " <= now: " << now << std::endl;
+            Write(1, "timeout");
+            return;
+        }
+        std::cout << "expire time(sec): " << expire << " > now: " << now << std::endl;
+        Write(1, send_string_);
+        return;
     }
 
 private:
     TestCode Read(int role) {
-        assert(role == 0 || role == 1); // 0 = client; 1 = server;
+//        assert(role == 0 || role == 1); // 0 = client; 1 = server;
 
         std::unique_lock<std::mutex> mutex(mutex_);
 
         std::cv_status wait_status;
         wait_status = cond_.wait_for(mutex, std::chrono::milliseconds(timeout_)) ;
         if (wait_status == std::cv_status::timeout) {
-            return TEST_TIMEOUT;
+            return TEST_ERR;
         }
 
         /*
@@ -91,26 +109,31 @@ private:
 
         assert(events[0].data.fd == sock_[role]);
          */
-        std::cout << "read" << std::endl;
-        auto readn = read(sock_[role], read_char, 3);
-        std::cout << "read sock: " << role << " buf: " << read_char<< " readn: " << readn << std::endl;
-        assert(readn == 3 && strcmp(read_char, "abc") == 0);
+        auto readn = read(sock_[role], recv_string_, strlen(expect_string_));
+        std::cout << "read sock: " << role << " recv string: " << recv_string_ << " readn: " << readn <<
+                  " expected string: " << expect_string_  << std::endl;
+        if (readn == strlen(expect_string_) && strcmp(recv_string_, expect_string_) == 0) {
+            if (strcmp(recv_string_, "timeout") == 0) {
+                return TEST_TIMEOUT;
+            } else {
+                return TEST_OK;
+            }
+        } else {
+            std::cout << "read unexpected string"<< std::endl;
+            return TEST_UNEXPECTED;
+        }
 
         /*
         close(ep_fd);
          */
-        return TEST_OK;
     }
 
-    void Write(int role) {
-        assert(role == 0 || role == 1); // 0 = client; 1 = server;
-
+    void Write(int role, const char* buf) {
         std::unique_lock<std::mutex> mutex(mutex_);
-        memcpy(write_char, "abc", 3);
 
-        auto writen = write(sock_[role], write_char, strlen(write_char));
-        std::cout << "write sock: " << role << " buf: " << write_char << " nwrite: " << writen << std::endl;
-        ASSERT_TRUE(writen == 3);
+        auto writen = write(sock_[role], buf, strlen(buf));
+        std::cout << "write sock: " << role << " send string: " << buf << " nwrite: " << writen << std::endl;
+        ASSERT_TRUE(writen == strlen(buf));
 
         cond_.notify_one();
     }
@@ -118,8 +141,9 @@ private:
 private:
     int timeout_;
     int sock_[2];
-    char read_char[8] = {0};
-    char write_char[8] = {0};
+    char recv_string_[8] = {0};// max used 7 bytes XXX
+    char send_string_[8] = "abc";
+    char expect_string_[8] = {0};
     std::mutex mutex_;
     std::condition_variable cond_;
 };
@@ -337,38 +361,98 @@ TEST(TestWatcherSet, AddAndDelKeyWatcher) {
 TEST(TestWatchServer, SimulateInteractive) {
     TestWatchServer server;
 
-    std::vector<Key*> keys0;
+    std::vector<Key *> keys0;
     keys0.push_back(new Key("k0.1"));
     keys0.push_back(new Key("k0.2"));
     keys0.push_back(new Key("k0.10"));
 
-    auto conn0 = new TestWatchConnection(5000); // read timeout 5s
+    {
+        auto conn0 = new TestWatchConnection(5000); // read timeout 5s
 
-    /*
-    // test timeout
-//    conn0->ServerWrite();
-    ASSERT_TRUE(conn0->ClientRead() == TEST_TIMEOUT); // no server write, timeout
-     */
+        /*
+        // test timeout
+    //    conn0->ServerWrite();
+        ASSERT_TRUE(conn0->ClientRead() == TEST_TIMEOUT); // no server write, timeout
+         */
 
-    auto w_ptr0 = std::make_shared<TestWatcher>(keys0, conn0);
-    WatcherPtr w_p0 = std::static_pointer_cast<Watcher>(w_ptr0);
-    server.AddKeyWatcher(w_p0);
-    ASSERT_TRUE(conn0->ClientRead() != TEST_TIMEOUT);
-
-    int i = 0;
-    int N = 1000;
-    std::thread th[N];
-    for (; i < N; i++) {
-        th[i] = std::thread([&]() {
-            auto conn = new TestWatchConnection(5000); // read timeout 5s
-            auto w_ptr = std::make_shared<TestWatcher>(keys0, conn);
-            WatcherPtr w_p = std::static_pointer_cast<Watcher>(w_ptr);
-            server.AddKeyWatcher(w_p);
-            ASSERT_TRUE(conn->ClientRead() != TEST_TIMEOUT);
-        });
+        auto w_ptr0 = std::make_shared<TestWatcher>(keys0, conn0);
+        WatcherPtr w_p0 = std::static_pointer_cast<Watcher>(w_ptr0);
+        server.AddKeyWatcher(w_p0);
+        ASSERT_TRUE(conn0->ClientRead() != TEST_TIMEOUT);
     }
-    for (i = 0; i < N; i++) {
-        th[i].join();
+
+    {
+        auto conn = new TestWatchConnection(5000, "hello", "timeout");
+        auto w_ptr = std::make_shared<TestWatcher>(keys0, conn);
+        WatcherPtr w_p = std::static_pointer_cast<Watcher>(w_ptr);
+        server.AddKeyWatcher(w_p);
+
+        ASSERT_TRUE(conn->ClientRead() == TEST_TIMEOUT);
+    }
+
+    {
+        auto conn = new TestWatchConnection(5000, "hello", "hello");
+        auto w_ptr = std::make_shared<TestWatcher>(keys0, conn);
+        WatcherPtr w_p = std::static_pointer_cast<Watcher>(w_ptr);
+        server.AddKeyWatcher(w_p);
+
+        sleep(1);
+        conn->ServerWrite(); // simulate server response
+
+        ASSERT_TRUE(conn->ClientRead() == TEST_OK);
+    }
+
+    {
+        auto conn = new TestWatchConnection(5000, "hello", "world");
+        auto w_ptr = std::make_shared<TestWatcher>(keys0, conn);
+        WatcherPtr w_p = std::static_pointer_cast<Watcher>(w_ptr);
+        server.AddKeyWatcher(w_p);
+
+        sleep(1);
+        conn->ServerWrite(); // simulate server response
+
+        ASSERT_TRUE(conn->ClientRead() == TEST_UNEXPECTED);
+    }
+
+    {
+        // test concurrent timeout response
+        int i;
+        int N = 1000;
+        int TEST_TIMES = 3;
+        std::thread th[N];
+        for (auto j = 0; j < TEST_TIMES; j++) {
+            for (i = 0; i < N; i++) {
+                th[i] = std::thread([&]() {
+                    auto conn = new TestWatchConnection(5000, "hello", "timeout"); // read timeout 5s
+                    auto w_ptr = std::make_shared<TestWatcher>(keys0, conn);
+                    WatcherPtr w_p = std::static_pointer_cast<Watcher>(w_ptr);
+                    server.AddKeyWatcher(w_p);
+                    ASSERT_TRUE(conn->ClientRead() == TEST_TIMEOUT);
+                });
+            }
+            for (i = 0; i < N; i++) {
+                th[i].join();
+            }
+        }
+
+        for (auto j = 0; j < TEST_TIMES; j++) {
+            for (i = 0; i < N; i++) {
+                th[i] = std::thread([&]() {
+                    auto conn = new TestWatchConnection(5000); // read timeout 5s
+                    auto w_ptr = std::make_shared<TestWatcher>(keys0, conn);
+                    WatcherPtr w_p = std::static_pointer_cast<Watcher>(w_ptr);
+                    server.AddKeyWatcher(w_p);
+
+                    sleep(1);
+                    conn->ServerWrite(); // simulate server response
+
+                    ASSERT_TRUE(conn->ClientRead() == TEST_OK);
+                });
+            }
+            for (i = 0; i < N; i++) {
+                th[i].join();
+            }
+        }
     }
 }
 
