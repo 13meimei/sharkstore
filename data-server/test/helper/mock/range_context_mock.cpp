63a3c941b8c3e0d60dc43feb2dc1fb34bc4c9114
byteink <jonahgaox@gmail.com>
2019-03-07 17:51:36 +0800

    fix unittest

--- data-server/test/helper/mock/range_context_mock.cpp
+++ data-server/test/helper/mock/range_context_mock.cpp
@@ -27,19 +27,20 @@ Status RangeContextMock::Init() {
         return Status(Status::kIOError, "mkdtemp", "");
     }
     // open rocksdb
-    db_ = new storage::RocksStore();
-//    path_ = path;
-//    rocksdb::Options ops;
-//    ops.create_if_missing = true;
-//    ops.create_missing_column_families = true;
-//    std::vector<rocksdb::ColumnFamilyDescriptor> column_families;
-//    column_families.emplace_back(rocksdb::kDefaultColumnFamilyName, rocksdb::ColumnFamilyOptions());
-//    column_families.emplace_back("txn", rocksdb::ColumnFamilyOptions());
-//    auto s = rocksdb::DB::Open(ops, JoinFilePath({path_, "data"}), column_families, &cf_handles_, &db_);
-//    if (!s.ok()) {
-//        return Status(Status::kIOError, "open rocksdb", s.ToString());
-//    }
-//    assert(cf_handles_.size() == 2);
+    rocksdb::DB *db = nullptr;
+    path_ = path;
+    rocksdb::Options ops;
+    ops.create_if_missing = true;
+    ops.create_missing_column_families = true;
+    std::vector<rocksdb::ColumnFamilyDescriptor> column_families;
+    column_families.emplace_back(rocksdb::kDefaultColumnFamilyName, rocksdb::ColumnFamilyOptions());
+    column_families.emplace_back("txn", rocksdb::ColumnFamilyOptions());
+    auto s = rocksdb::DB::Open(ops, JoinFilePath({path_, "data"}), column_families, &cf_handles_, &db);
+    if (!s.ok()) {
+        return Status(Status::kIOError, "open rocksdb", s.ToString());
+    }
+    assert(cf_handles_.size() == 2);
+    db_ = new storage::RocksStore(db, cf_handles_[1]);
 
     // open meta db
     meta_store_.reset(new storage::MetaStore(JoinFilePath({path_, "meta"})));
@@ -65,9 +66,8 @@ Status RangeContextMock::Init() {
 }
 
 void RangeContextMock::Destroy() {
-    for (auto handle : cf_handles_) {
-        delete handle;
-    }
+    delete db_;
+    db_ = nullptr;
     if (!path_.empty()) {
         RemoveDirAll(path_.c_str());
     }
