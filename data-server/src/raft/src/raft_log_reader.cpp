70aa9f8200508fc277f05be889633b7aaf69f0f8
zhangyongcheng2 <zhangyongcheng1@jd.com>
2019-03-28 14:06:42 +0800

    update

---
+++ data-server/src/raft/src/raft_log_reader.cpp
@@ -0,0 +1,18 @@
+#include "raft/raft_log_reader.h"
+#include "impl/storage_reader.h"
+
+namespace sharkstore {
+namespace raft {
+
+std::shared_ptr<RaftLogReader> CreateRaftLogReader(const uint64_t id,
+                                                   const std::function<bool(const std::string&)>& f0,
+                                                   const std::function<bool(const metapb::Range &meta)>& f1,
+                                                   RaftServerImpl *server,
+                                                   sharkstore::dataserver::storage::DbInterface* db,
+                                                   sharkstore::raft::impl::WorkThread* trd)
+{
+    return std::shared_ptr<RaftLogReader>(new impl::StorageReader(id, f0, f1, server, db, trd));
+}
+
+} /* namespace raft */
+} /* namespace sharkstore */
