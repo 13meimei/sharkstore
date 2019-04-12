00e874a187d266f3e07d60884f021ab1057c366a
byteink <jonahgaox@gmail.com>
2019-04-12 11:05:43 +0800

    fix fetch available memory

--- data-server/src/monitor/linux_status.cpp
+++ data-server/src/monitor/linux_status.cpp
@@ -1,6 +1,7 @@
 #include "linux_status.h"
 
 #include <sys/vfs.h>
+#include <stdio.h>
 #include <sys/sysinfo.h>
 #include <string.h>
 
@@ -21,14 +22,36 @@ bool LinuxStatus::GetFileSystemUsage(const char *path, uint64_t *total, uint64_t
 }
 
 bool LinuxStatus::GetMemoryUsage(uint64_t *total, uint64_t *available) {
-    struct sysinfo si;
-    if (::sysinfo(&si) != 0) {
-        perror("call sysinfo failed:");
+    const char* kTotalName = "MemTotal";
+    const char* kAvailableName = "MemAvailable";
+
+    FILE* fp = fopen("/proc/meminfo", "r");
+    if (fp == NULL) {
+        perror("open /proc/meminfo");
         return false;
     }
-    *total = si.totalram;
-    *available = si.freeram + si.bufferram;
-    return true;
+
+    bool find_total = false, find_avail = false;
+    int ret = 0;
+    while (!find_total || !find_avail) {
+        char name[256] = {'\0'};
+        unsigned long val = 0;
+        ret = fscanf(fp, "%s %lu", name, &val);
+        if (ret <= 0) {
+            if (ret < 0) {
+                perror("fscanf /pro/meminfo");
+            }
+            break;
+        }
+        if (strncmp(name, kTotalName, strlen(kTotalName)) == 0) {
+            *total = val;
+            find_total = true;
+        } else if (strncmp(name, kAvailableName, strlen(kAvailableName)) == 0) {
+            *available = val;
+            find_avail = true;
+        }
+    }
+    return find_total && find_avail;
 }
 
 }
